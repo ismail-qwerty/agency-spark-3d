457b583e20bf29611ba970c00f0989a054855ce5
ismail-qwerty <ismailunique8@gmail.com>
2026-04-10 05:13:42 +0500

    Add Project and TeamMember interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,28 @@
+export interface Project {
+  client: string;
+  category: string;
+  year: string;
+  rating: number;
+  quote: string;
+  color: string;
+  description: string;
+  challenge: string;
+  solution: string;
+  results: string[];
+  tools: string[];
+  duration: string;
+}
 
+export interface TeamMember {
+  name: string;
+  role: string;
+  badge: string;
+  initials: string;
+  image?: string;
+  bio: string;
+  email: string;
+  isLead: boolean;
+  skills: string[];
+  experience: string;
+  linkedin?: string;
+}
